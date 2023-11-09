df512555bddb674291a6c050fc1dabf66f9648af
Lucas Santos <lhs.santoss@gmail.com>
2023-11-09 23:53:10 +0100

    Fix Typo
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/data/ClassRepository.ts
+++ src/data/ClassRepository.ts
@@ -1,6 +1,5 @@
 import { Class } from '../domain/Class.js'
 import { Database } from './Db.js'
-import { TeacherRepository } from './TeacherRepository.js'
 
 export class ClassRepository extends Database {
   constructor() {
